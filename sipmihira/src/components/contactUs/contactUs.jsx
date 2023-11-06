154bc5c8f2e8f2755d2ecda2a8da80f5d7f5f23d
sandarudivyantha <sandarudivyantha@gmail.com>
2023-11-07 01:00:25 +0530

    contact us updated

--- sipmihira/src/components/contactUs/contactUs.jsx
+++ sipmihira/src/components/contactUs/contactUs.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import styled from "styled-components";
-
-// npm i @emailjs/browser
+import "./contactUs.css"; // Import the external CSS file
 
 const ContactUs = () => {
   const form = useRef();
@@ -21,6 +19,7 @@ const ContactUs = () => {
         (result) => {
           console.log(result.text);
           console.log("message sent");
+          form.current.reset();
         },
         (error) => {
           console.log(error.text);
@@ -29,72 +28,35 @@ const ContactUs = () => {
   };
 
   return (
-    <StyledContactForm id="contactUs">
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
-    </StyledContactForm>
+    <div className="gpt3__blog section__padding" id="contactUs">
+      <div className="gpt3__contactUs-container">
+        <h2 className="gradient__text">Contact Us</h2>
+        <div className="gpt3__contactUs-container_groupA">
+          <div className="StyledContactForm">
+            <form ref={form} onSubmit={sendEmail}>
+              <label className="formLableHeading">Name</label>
+              <input type="text" name="user_name" className="inputFild" />
+              <label className="formLableHeading">Email</label>
+              <input type="email" name="user_email" />
+              <label className="formLableHeading">Message</label>
+              <textarea name="message" className="custom-textarea" />
+              <input type="submit" value="Send" />
+            </form>
+          </div>
+
+          <div class="contactDetails">
+            <div>
+              <p className="contactDetails-p1"><strong>Contact Details</strong></p><br />
+              <p className="gradient__text p2">"Your feedback is invaluable to us. Get in touch anytime."</p><br />
+              <p className="contactDetails-p3">Hope this project helped you in some manner. Thank you!</p><br />
+              <p className="contactDetails-p3"><b>-Team Sipmihira-	</b></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default ContactUs;
-
-// Styles
-const StyledContactForm = styled.div`
-  width: 400px;
-
-  form {
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    width: 100%;
-    font-size: 16px;
-
-    input {
-      width: 100%;
-      height: 35px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-
-    textarea {
-      max-width: 100%;
-      min-width: 100%;
-      width: 100%;
-      max-height: 100px;
-      min-height: 100px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-
-    label {
-      margin-top: 1rem;
-    }
-
-    input[type="submit"] {
-      margin-top: 2rem;
-      cursor: pointer;
-      background: rgb(249, 105, 14);
-      color: white;
-      border: none;
-    }
-  }
-`;
