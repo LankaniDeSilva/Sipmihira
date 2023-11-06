15de4dd29cb4944a6fdc3a7584d82c60b83f3709
sandarudivyantha <sandarudivyantha@gmail.com>
2023-11-06 12:38:32 +0530

    Aout Us

--- sipmihira/src/components/article/Article.jsx
+++ sipmihira/src/components/article/Article.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = ({ imgUrl, date, text }) => (
+const Article = ({ imgUrl, name, status, possision, campus, department, department_possion }) => (
   <div className="gpt3__blog-container_article">
     <div className="gpt3__blog-container_article-image">
       <img src={imgUrl} alt="blog_image" />
     </div>
     <div className="gpt3__blog-container_article-content">
       <div>
-        <p>{date}</p>
-        <h3>{text}</h3>
+        <p>{name}</p>
+        <h3>{status}</h3><br/>
+        <h4>{possision}</h4>
+        <p2>{campus}</p2><br/>
+        <h4>{department}</h4>
+        <p2>{department_possion}</p2><br/>
       </div>
-      <p>Read Full Article</p>
     </div>
   </div>
 );
--- sipmihira/src/containers/blog/Blog.jsx
+++ sipmihira/src/containers/blog/Blog.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Article from '../../components/article/Article';
-import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import { blog01, blog02, blog03, blog04, blog05, blog06 } from './imports';
 import './blog.css';
 
 const Blog = () => (
   <div className="gpt3__blog section__padding" id="blog">
     <div className="gpt3__blog-heading">
-      <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      <h2 className="gradient__text h2">About Us <br /> </h2>
+      <h3 className="subHeadindg2 h3">Meet Our Team !</h3>
     </div>
     <div className="gpt3__blog-container">
-      <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-      </div>
-
       <div className="gpt3__blog-container_groupB">
-        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog01} name="Ms. Suriyaa Kumari" status="Supervisor" possision="Lecturer" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Faculty Of Computing | Information Technology"/>
+        <Article imgUrl={blog02} name="Mr. Supunya Swarnakantha" status="Co-Supervisor" possision="Lecturer" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Faculty Of Computing | Information Technology"/>
+        <Article imgUrl={blog03} name="Saranga R.G.V." status="Developer" possision="Undergraduate" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Computer Science & Software Engineering"/>
+        <Article imgUrl={blog04} name="Divyantha U.A.S." status="Developer" possision="Undergraduate" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Computer Science & Software Engineering"/>
+        <Article imgUrl={blog05} name="Jayasinghe A.H.N." status="Developer" possision="Undergraduate" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Computer Science & Software Engineering"/>
+        <Article imgUrl={blog06} name="Lankani de Silva" status="Developer" possision="Undergraduate" campus="Sri Lanka Institute of Information Technology" department="Department" department_possion="Computer Science & Software Engineering"/>
       </div>
     </div>
   </div>
