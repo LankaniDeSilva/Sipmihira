edae9c8bb8e69ce4f38bbb3531a0d032487e1763
sandarudivyantha <sandarudivyantha@gmail.com>
2023-11-08 11:08:02 +0530

    Blog Page name changed to aboutus and rows fixed

--- sipmihira/src/containers/index.js
+++ sipmihira/src/containers/index.js
@@ -1,4 +1,4 @@
-export { default as Blog } from './blog/Blog';
+export { default as AboutUs } from './aboutUs/AboutUs';
 export { default as Downloads } from './downloads/Downloads';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
